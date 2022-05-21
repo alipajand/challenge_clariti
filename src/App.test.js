e67c1430971eb2ce91fa7e15168dfc21a5977a54
Ali Pajand <ali.pajand@gmail.com>
2022-05-21 15:01:16 -0400

    :art: Write some tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,33 @@
 import {render, screen} from '@testing-library/react'
 import App from './App'
+import FileReader from './components/FileReader'
 
-test('renders learn react link', () => {
+test('renders header', () => {
   render(<App />)
-  const linkElement = screen.getByText(/learn react/i)
+  const linkElement = screen.getByText(/Code Challenge/)
+  expect(linkElement).toBeInTheDocument()
+})
+
+test('renders header class name', () => {
+  render(<App />)
+  const linkElement = screen.getByText(/Code Challenge/)
+  expect(linkElement).toHaveClass('app-logo-text')
+})
+
+test('renders logo', () => {
+  render(<App />)
+  const linkElement = screen.getByAltText(/logo/)
+  expect(linkElement).toBeInTheDocument()
+})
+
+test('renders logo class name', () => {
+  render(<App />)
+  const linkElement = screen.getByAltText(/logo/)
+  expect(linkElement).toHaveClass('app-logo')
+})
+
+test('renders select a csv file to start', () => {
+  render(<FileReader />)
+  const linkElement = screen.getByText(/Select a CSV file to start/i)
   expect(linkElement).toBeInTheDocument()
 })
